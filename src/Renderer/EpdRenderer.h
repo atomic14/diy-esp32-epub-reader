8d7e1c8bf4c8db4ff83cb77ea7fd4a943cad6718
Chris Greening <chris@cmgresearch.com>
2021-09-17 08:52:12 +0100

    New fonts

--- src/Renderer/EpdRenderer.h
+++ src/Renderer/EpdRenderer.h
@@ -2,20 +2,23 @@
 #include <esp_log.h>
 #include <epd_driver.h>
 #include <epd_highlevel.h>
-#include "firasans.h"
 #include "Renderer.h"
 #include "epdiy_ED047TC1.h"
 
 class EpdRenderer : public Renderer
 {
 private:
-  const EpdFont *m_font;
+  const EpdFont *m_regular_font;
+  const EpdFont *m_bold_font;
+  const EpdFont *m_italic_font;
+  const EpdFont *m_bold_italic_font;
   EpdiyHighlevelState m_hl;
   uint8_t *m_frame_buffer;
   EpdFontProperties m_font_props;
 
 public:
-  EpdRenderer()
+  EpdRenderer(const EpdFont *regular_font, const EpdFont *bold_font, const EpdFont *italic_font, const EpdFont *bold_italic_font)
+      : m_regular_font(regular_font), m_bold_font(bold_font), m_italic_font(italic_font), m_bold_italic_font(bold_italic_font)
   {
     m_font_props = epd_font_properties_default();
     // start up the EPD
@@ -25,7 +28,6 @@ public:
     epd_hl_set_all_white(&m_hl);
     epd_set_rotation(EPD_ROT_INVERTED_PORTRAIT);
     m_frame_buffer = epd_hl_get_framebuffer(&m_hl);
-    m_font = &FiraSans;
     epd_clear();
   }
   ~EpdRenderer() {}
@@ -33,14 +35,14 @@ public:
   {
     get_text(src, start_index, end_index);
     int x = 0, y = 0, x1 = 0, y1 = 0, x2 = 0, y2 = 0;
-    epd_get_text_bounds(m_font, buffer, &x, &y, &x1, &y1, &x2, &y2, &m_font_props);
+    epd_get_text_bounds(m_regular_font, buffer, &x, &y, &x1, &y1, &x2, &y2, &m_font_props);
     return x2 - x1;
   }
   void draw_text(int x, int y, const char *src, int start_index, int end_index, bool italic = false, bool bold = false)
   {
     get_text(src, start_index, end_index);
     int ypos = y + get_line_height();
-    epd_write_string(m_font, buffer, &x, &ypos, m_frame_buffer, &m_font_props);
+    epd_write_string(m_regular_font, buffer, &x, &ypos, m_frame_buffer, &m_font_props);
   }
   void draw_rect(int x, int y, int width, int height, uint8_t color = 0)
   {
@@ -85,6 +87,6 @@ public:
   }
   virtual int get_line_height()
   {
-    return m_font->advance_y;
+    return m_regular_font->advance_y;
   }
 };
--- src/main.cpp
+++ src/main.cpp
@@ -11,6 +11,10 @@
 #include "Renderer/ConsoleRenderer.h"
 #include <list>
 #include <string.h>
+#include "Fonts/lato-regular.h"
+#include "Fonts/lato-bold.h"
+#include "Fonts/lato-italic.h"
+#include "Fonts/lato-bold-italic.h"
 
 extern "C"
 {
@@ -21,7 +25,7 @@ void main_task(void *param)
 {
   ESP_LOGI("main", "Memory before renderer init: %d", esp_get_free_heap_size());
   // create the EPD renderer
-  EpdRenderer *renderer = new EpdRenderer();
+  EpdRenderer *renderer = new EpdRenderer(&lato_regular, &lato_bold, &lato_italic, &lato_bold_italic);
   ESP_LOGI("main", "Memory after renderer init: %d", esp_get_free_heap_size());
   // initialise the SDCard
   SDCard *sdcard = new SDCard("/sdcard", PIN_NUM_MISO, PIN_NUM_MOSI, PIN_NUM_CLK, PIN_NUM_CS);
