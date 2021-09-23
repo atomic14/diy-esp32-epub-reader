66e2ddbe3c1fb18d6cd41284768a06e87c72ac20
Chris Greening <chris@cmgresearch.com>
2021-09-23 20:43:42 +0100

    Refactor controls and deep sleep

---
+++ src/controls/Controls.cpp
@@ -0,0 +1,154 @@
+#include <esp_sleep.h>
+#include <driver/rtc_io.h>
+#include <driver/gpio.h>
+#include <esp32/ulp.h>
+#include <esp_log.h>
+#include "Controls.h"
+#include "ulp_main.h"
+
+extern const uint8_t ulp_main_bin_start[] asm("_binary_ulp_main_bin_start");
+extern const uint8_t ulp_main_bin_end[] asm("_binary_ulp_main_bin_end");
+
+Controls::Controls(gpio_num_t up, gpio_num_t down, gpio_num_t select, int active_level) : up(up), down(down), select(select), active_level(active_level)
+{
+}
+
+void Controls::setup_inputs()
+{
+  gpio_set_direction(up, GPIO_MODE_INPUT);
+  gpio_set_direction(down, GPIO_MODE_INPUT);
+  gpio_set_direction(select, GPIO_MODE_INPUT);
+
+  if (active_level == 0)
+  {
+    gpio_set_pull_mode(up, GPIO_PULLUP_ONLY);
+    gpio_set_pull_mode(down, GPIO_PULLUP_ONLY);
+    gpio_set_pull_mode(select, GPIO_PULLUP_ONLY);
+  }
+  else
+  {
+    gpio_set_pull_mode(up, GPIO_PULLDOWN_ONLY);
+    gpio_set_pull_mode(down, GPIO_PULLDOWN_ONLY);
+    gpio_set_pull_mode(select, GPIO_PULLDOWN_ONLY);
+  }
+}
+
+UIAction Controls::get_action()
+{
+  if (gpio_get_level(up) == active_level)
+  {
+    return UIAction::UP;
+  }
+  if (gpio_get_level(down) == active_level)
+  {
+    return UIAction::DOWN;
+  }
+  if (gpio_get_level(select) == active_level)
+  {
+    return UIAction::SELECT;
+  }
+  return UIAction::NONE;
+}
+
+bool Controls::did_wake_from_deep_sleep()
+{
+  auto wake_cause = esp_sleep_get_wakeup_cause();
+  // if our controls are active low then we must have been woken by the ULP
+  // as that's the only mechanism available for active low buttons
+  if (active_level == 0)
+  {
+    ESP_LOGI("Controls", "ULP Wakeup");
+    return wake_cause == ESP_SLEEP_WAKEUP_ULP;
+  }
+  if (wake_cause == ESP_SLEEP_WAKEUP_EXT1)
+  {
+    ESP_LOGI("Controls", "EXT1 Wakeup");
+    return true;
+  }
+  return false;
+}
+
+UIAction Controls::get_deep_sleep_action()
+{
+  if (active_level == 0)
+  {
+    uint16_t rtc_pin = ulp_gpio_status & UINT16_MAX;
+    ESP_LOGI("Controls", "***** rtc_pin: %d", rtc_pin);
+    if ((rtc_pin & (1 << rtc_io_number_get(up))))
+    {
+      ESP_LOGI("Controls", "***** UP %d, %d, %d", 1 << rtc_io_number_get(up), rtc_pin, (rtc_pin & (1 << rtc_io_number_get(up))));
+      return UIAction::UP;
+    }
+    else if ((rtc_pin & (1 << rtc_io_number_get(down))))
+    {
+      ESP_LOGI("Controls", "***** DOWN %d, %d, %d", 1 << rtc_io_number_get(down), rtc_pin, (rtc_pin & (1 << rtc_io_number_get(down))));
+      return UIAction::DOWN;
+    }
+    else if ((rtc_pin & (1 << rtc_io_number_get(select))))
+    {
+      ESP_LOGI("Controls", "***** SELECT %d, %d, %d", 1 << rtc_io_number_get(select), rtc_pin, (rtc_pin & (1 << rtc_io_number_get(select))));
+      return UIAction::SELECT;
+    }
+  }
+  else
+  {
+    uint64_t ext1_buttons = esp_sleep_get_ext1_wakeup_status();
+    if (ext1_buttons & (1ULL << up))
+    {
+      return UIAction::UP;
+    }
+    else if (ext1_buttons & (1ULL << down))
+    {
+      return UIAction::DOWN;
+    }
+    else if (ext1_buttons & (1ULL << select))
+    {
+      return UIAction::SELECT;
+    }
+  }
+  return UIAction::NONE;
+}
+
+void Controls::setup_deep_sleep()
+{
+  if (active_level == 0)
+  {
+    rtc_gpio_init(up);
+    rtc_gpio_set_direction(up, RTC_GPIO_MODE_INPUT_ONLY);
+    rtc_gpio_pulldown_dis(up);
+    rtc_gpio_pullup_dis(up);
+    rtc_gpio_hold_dis(up);
+
+    rtc_gpio_init(down);
+    rtc_gpio_set_direction(down, RTC_GPIO_MODE_INPUT_ONLY);
+    rtc_gpio_pulldown_dis(down);
+    rtc_gpio_pullup_dis(down);
+    rtc_gpio_hold_dis(down);
+
+    rtc_gpio_init(select);
+    rtc_gpio_set_direction(select, RTC_GPIO_MODE_INPUT_ONLY);
+    rtc_gpio_pulldown_dis(select);
+    rtc_gpio_pullup_dis(select);
+    rtc_gpio_hold_dis(select);
+    // need to use the ULP if we have buttons that are active low
+    // see ulp/main.S for more details
+
+    esp_err_t err = ulp_load_binary(0, ulp_main_bin_start, (ulp_main_bin_end - ulp_main_bin_start) / sizeof(uint32_t));
+    ESP_ERROR_CHECK(err);
+
+    ulp_up_mask = 1 << rtc_io_number_get(up);
+    ulp_down_mask = 1 << rtc_io_number_get(down);
+    ulp_select_mask = 1 << rtc_io_number_get(select);
+
+    ulp_set_wakeup_period(0, 100 * 1000); // 100 ms
+    err = ulp_run(&ulp_entry - RTC_SLOW_MEM);
+    ESP_ERROR_CHECK(err);
+  }
+  else
+  {
+    // can use ext1 for buttons that are active high
+    esp_sleep_enable_ext1_wakeup(
+        (1ULL << up) | (1ULL << down) | (1ULL << select),
+        ESP_EXT1_WAKEUP_ANY_HIGH);
+  }
+}
--- src/main.cpp
+++ src/main.cpp
@@ -1,11 +1,7 @@
 #include <freertos/FreeRTOS.h>
 #include <freertos/task.h>
-#include <driver/rtc_io.h>
-#include <driver/gpio.h>
 #include <esp_sleep.h>
 #include <esp_log.h>
-#include <esp_sleep.h>
-#include <esp32/ulp.h>
 #include "config.h"
 #include "SDCard.h"
 #include "EpubList/Epub.h"
@@ -20,10 +16,7 @@
 #include <bold_font.h>
 #include <italic_font.h>
 #include <bold_italic_font.h>
-#include "ulp_main.h"
-
-extern const uint8_t ulp_main_bin_start[] asm("_binary_ulp_main_bin_start");
-extern const uint8_t ulp_main_bin_end[] asm("_binary_ulp_main_bin_end");
+#include "controls/Controls.h"
 
 extern "C"
 {
@@ -50,49 +43,8 @@ const gpio_num_t BUTTON_UP_GPIO_NUM = GPIO_NUM_34;     // RTC 4
 const gpio_num_t BUTTON_DOWN_GPIO_NUM = GPIO_NUM_39;   // RTC 3
 const gpio_num_t BUTTON_SELECT_GPIO_NUM = GPIO_NUM_35; // RTC 5
 
-typedef enum
-{
-  NONE,
-  UP,
-  DOWN,
-  SELECT
-} UIAction;
-
-void handleEpubList(Renderer *renderer, UIAction action);
 void handleEpub(Renderer *renderer, UIAction action);
-
-static void init_ulp_program()
-{
-  esp_err_t err = ulp_load_binary(0, ulp_main_bin_start, (ulp_main_bin_end - ulp_main_bin_start) / sizeof(uint32_t));
-  ESP_ERROR_CHECK(err);
-
-  gpio_num_t gpio_p1 = BUTTON_UP_GPIO_NUM;
-  gpio_num_t gpio_p2 = BUTTON_DOWN_GPIO_NUM;
-  gpio_num_t gpio_p3 = BUTTON_SELECT_GPIO_NUM;
-
-  rtc_gpio_init(gpio_p1);
-  rtc_gpio_set_direction(gpio_p1, RTC_GPIO_MODE_INPUT_ONLY);
-  rtc_gpio_pulldown_dis(gpio_p1);
-  rtc_gpio_pullup_dis(gpio_p1);
-  rtc_gpio_hold_dis(gpio_p1);
-
-  rtc_gpio_init(gpio_p2);
-  rtc_gpio_set_direction(gpio_p2, RTC_GPIO_MODE_INPUT_ONLY);
-  rtc_gpio_pulldown_dis(gpio_p2);
-  rtc_gpio_pullup_dis(gpio_p2);
-  rtc_gpio_hold_dis(gpio_p2);
-
-  rtc_gpio_init(gpio_p3);
-  rtc_gpio_set_direction(gpio_p3, RTC_GPIO_MODE_INPUT_ONLY);
-  rtc_gpio_pulldown_dis(gpio_p3);
-  rtc_gpio_pullup_dis(gpio_p3);
-  rtc_gpio_hold_dis(gpio_p3);
-
-  ulp_set_wakeup_period(0, 100 * 1000); // 100 ms
-  err = ulp_run(&ulp_entry - RTC_SLOW_MEM);
-
-  ESP_ERROR_CHECK(err);
-}
+void handleEpubList(Renderer *renderer, UIAction action);
 
 EpubReader *reader = nullptr;
 void handleEpub(Renderer *renderer, UIAction action)
@@ -114,6 +66,9 @@ void handleEpub(Renderer *renderer, UIAction action)
     // switch back to main screen
     ui_state = SELECTING_EPUB;
     renderer->clear_screen();
+    // clear the epub reader away
+    delete reader;
+    reader = nullptr;
     // force a redraw
     epub_list_state.previous_rendered_page = -1;
     handleEpubList(renderer, NONE);
@@ -152,6 +107,8 @@ void handleEpubList(Renderer *renderer, UIAction action)
     epub_reader_state.current_section = 0;
     epub_reader_state.current_page = 0;
     ui_state = READING_EPUB;
+    reader = new EpubReader(epub_reader_state, renderer);
+
     renderer->clear_screen();
     renderer->flush_display();
     handleEpub(renderer, NONE);
@@ -180,6 +137,7 @@ void handleUserInteraction(Renderer *renderer, UIAction ui_action)
 
 void main_task(void *param)
 {
+  // need to power on the EDP to get power to the SD Card
   epd_poweron();
   ESP_LOGI("main", "Memory before renderer init: %d", esp_get_free_heap_size());
   // create the EPD renderer
@@ -189,69 +147,41 @@ void main_task(void *param)
   // initialise the SDCard
   SDCard *sdcard = new SDCard("/sdcard", PIN_NUM_MISO, PIN_NUM_MOSI, PIN_NUM_CLK, PIN_NUM_CS);
   ESP_LOGI("main", "Memory after sdcard init: %d", esp_get_free_heap_size());
+  // set the controls up
+  Controls *controls = new Controls(BUTTON_UP_GPIO_NUM, BUTTON_DOWN_GPIO_NUM, BUTTON_SELECT_GPIO_NUM, 0);
 
-  // work out if we were woken via EXT1 and which button was pressed
-  if (esp_sleep_get_wakeup_cause() == ESP_SLEEP_WAKEUP_ULP)
+  // work out if we were woken from deep sleep
+  if (controls->did_wake_from_deep_sleep())
   {
     // restore the renderer state - it should have been saved when we went to sleep...
     renderer->hydrate();
 
-    uint16_t rtc_pin = ulp_gpio_status & UINT16_MAX;
-    ESP_LOGI("main", "Woken by ULP %d", rtc_pin);
-    if (rtc_pin == rtc_io_number_get(BUTTON_UP_GPIO_NUM))
-    {
-      ESP_LOGI("main", "Button UP pressed");
-      handleUserInteraction(renderer, UP);
-    }
-    else if (rtc_pin == rtc_io_number_get(BUTTON_DOWN_GPIO_NUM))
-    {
-      ESP_LOGI("main", "Button DOWN pressed");
-      handleUserInteraction(renderer, DOWN);
-    }
-    else if (rtc_pin == rtc_io_number_get(BUTTON_SELECT_GPIO_NUM))
-    {
-      ESP_LOGI("main", "Button SELECT pressed");
-      handleUserInteraction(renderer, SELECT);
-    }
-    else
-    {
-      handleUserInteraction(renderer, NONE);
-    }
+    UIAction ui_action = controls->get_deep_sleep_action();
+    handleUserInteraction(renderer, ui_action);
   }
   else
   {
-    ESP_LOGI(TAG, "Initialise ULP program");
-    init_ulp_program();
-
+    // reset the screen
     renderer->reset();
+    // make sure the UI is in the right state
     handleUserInteraction(renderer, NONE);
   }
-
-  gpio_set_direction(BUTTON_UP_GPIO_NUM, GPIO_MODE_INPUT);
-  gpio_set_direction(BUTTON_DOWN_GPIO_NUM, GPIO_MODE_INPUT);
-  gpio_set_direction(BUTTON_SELECT_GPIO_NUM, GPIO_MODE_INPUT);
-
+  // configure the button inputs
+  controls->setup_inputs();
+  // keep track of when the user last interacted and go to sleep after 30 seconds
   int64_t last_user_interaction = esp_timer_get_time();
   while (esp_timer_get_time() - last_user_interaction < 30 * 1000 * 1000)
   {
-    // check to see if up is presses
-    if (gpio_get_level(BUTTON_DOWN_GPIO_NUM) == 0)
+    // check for user interaction
+    UIAction ui_action = controls->get_action();
+    if (ui_action != NONE)
     {
-      handleUserInteraction(renderer, DOWN);
       last_user_interaction = esp_timer_get_time();
-    }
-    else if (gpio_get_level(BUTTON_UP_GPIO_NUM) == 0)
-    {
-      handleUserInteraction(renderer, UP);
-      last_user_interaction = esp_timer_get_time();
-    }
-    else if (gpio_get_level(BUTTON_SELECT_GPIO_NUM) == 0)
-    {
-      handleUserInteraction(renderer, SELECT);
-      last_user_interaction = esp_timer_get_time();
+      handleUserInteraction(renderer, ui_action);
     }
     else
     {
+      // wait for the user to do something
       vTaskDelay(pdMS_TO_TICKS(50));
     }
   }
@@ -259,10 +189,14 @@ void main_task(void *param)
   ESP_LOGI("main", "Saving state");
   // save the state
   renderer->dehydrate();
-
+  // turn off the SD Card
+  delete sdcard;
   ESP_ERROR_CHECK(esp_sleep_enable_ulp_wakeup());
   ESP_LOGI("main", "Entering deep sleep");
   epd_poweroff();
+  // configure deep sleep options
+  controls->setup_deep_sleep();
+  // go to sleep
   esp_deep_sleep_start();
 }
 
