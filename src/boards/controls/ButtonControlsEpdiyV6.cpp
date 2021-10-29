4e98364425cc7b42cae4f44121e76c28bba48454
martinberlin <martin@luckycloud.de>
2021-10-29 18:15:36 +0200

    Cleanup after merging #64 Thanks @atomic14 !

--- src/boards/controls/ButtonControlsEpdiyV6.cpp
+++ src/boards/controls/ButtonControlsEpdiyV6.cpp
@@ -7,58 +7,6 @@
 #include "ButtonControlsEpdiyV6.h"
 #include "ulp_main.h"
 
-// 100 ms debounce on the buttons
-const int BUTTON_DEBOUNCE = 1000;
-
-IRAM_ATTR void button_interrupt_handler(void *param)
-{
-  Button *button = (Button *)param;
-  button->handle_interrupt();
-}
-
-Button::Button(gpio_num_t gpio_pin, int active_level, ButtonCallback_t callback)
-    : gpio_pin(gpio_pin),
-      active_level(active_level),
-      callback(callback)
-{
-  // setup the pin
-  gpio_set_direction(gpio_pin, GPIO_MODE_INPUT);
-  gpio_set_pull_mode(gpio_pin, active_level == 0 ? GPIO_PULLUP_ONLY : GPIO_PULLDOWN_ONLY);
-  // wire up the interrupt handler
-  gpio_isr_handler_add(gpio_pin, button_interrupt_handler, this);
-  // start off waiting for the button to be pressed - edge interrupts seem to be flaky so we
-  // have to be a bit more clever
-  button_pressed = false;
-  gpio_set_intr_type(gpio_pin, active_level == 0 ? GPIO_INTR_LOW_LEVEL : GPIO_INTR_HIGH_LEVEL);
-  gpio_intr_enable(gpio_pin);
-}
-
-void Button::handle_interrupt()
-{
-  if (button_pressed)
-  {
-    // the button was pressed - so now it must have been released
-    button_pressed = false;
-    int64_t button_release_time = esp_timer_get_time();
-    // has enough time passed to call this a button press?
-    if (button_release_time - button_press_start > BUTTON_DEBOUNCE)
-    {
-      // call the callback
-      callback();
-    }
-    // start listening for the button to be pressed again
-    gpio_set_intr_type(gpio_pin, active_level == 0 ? GPIO_INTR_LOW_LEVEL : GPIO_INTR_HIGH_LEVEL);
-  }
-  else
-  {
-    // the button was not pressed - so now it must be pressed
-    button_pressed = true;
-    button_press_start = esp_timer_get_time();
-    // start listening for the button to be released
-    gpio_set_intr_type(gpio_pin, active_level == 0 ? GPIO_INTR_HIGH_LEVEL : GPIO_INTR_LOW_LEVEL);
-  }
-}
-
 extern const uint8_t ulp_main_bin_start[] asm("_binary_ulp_main_bin_start");
 extern const uint8_t ulp_main_bin_end[] asm("_binary_ulp_main_bin_end");
 
--- src/boards/controls/ButtonControlsEpdiyV6.h
+++ src/boards/controls/ButtonControlsEpdiyV6.h
@@ -2,31 +2,10 @@
 #include "pca9555.h"
 #include <driver/i2c.h>
 #include "Actions.h"
-#include <hal/gpio_types.h>
+#include "Button.h"
 
 #define REG_INPUT_PORT0      0
 #define BUTTON_IO_DOWN  (PCA_PIN_P07 >> 8)
-
-typedef std::function<void(void)> ButtonCallback_t;
-
-class Button
-{
-private:
-  gpio_num_t gpio_pin;
-  int active_level;
-
-  friend void button_interrupt_handler(void *param);
-
-  int64_t button_press_start = 0;
-  bool button_pressed = false;
-
-  ButtonCallback_t callback;
-
-  void handle_interrupt();
-
-public:
-  Button(gpio_num_t gpio_pin, int active_level, ButtonCallback_t callback);
-};
 
 class ButtonControls
 {
