ecdc9dca998a689fe4dcfa86c2e6679592114332
Chris Greening <chris@cmgresearch.com>
2021-12-08 10:13:35 +0000

    Adds cyrillic code points

--- scripts/generate_fonts.sh
+++ scripts/generate_fonts.sh
@@ -2,13 +2,13 @@
 
 python3 -m venv venv
 . ./venv/bin/activate
-# pip install --upgrade pip
-# pip install freetype-py
+pip install --upgrade pip
+pip install freetype-py
 
-# curl https://raw.githubusercontent.com/google/fonts/main/ofl/sourcesanspro/SourceSansPro-Regular.ttf -o SourceSansPro-Regular.ttf
-# curl https://raw.githubusercontent.com/google/fonts/main/ofl/sourcesanspro/SourceSansPro-Bold.ttf -o SourceSansPro-Bold.ttf
-# curl https://raw.githubusercontent.com/google/fonts/main/ofl/sourcesanspro/SourceSansPro-Italic.ttf -o SourceSansPro-Italic.ttf
-# curl https://raw.githubusercontent.com/google/fonts/main/ofl/sourcesanspro/SourceSansPro-BoldItalic.ttf -o SourceSansPro-BoldItalic.ttf
+curl https://raw.githubusercontent.com/google/fonts/main/ofl/sourcesanspro/SourceSansPro-Regular.ttf -o SourceSansPro-Regular.ttf
+curl https://raw.githubusercontent.com/google/fonts/main/ofl/sourcesanspro/SourceSansPro-Bold.ttf -o SourceSansPro-Bold.ttf
+curl https://raw.githubusercontent.com/google/fonts/main/ofl/sourcesanspro/SourceSansPro-Italic.ttf -o SourceSansPro-Italic.ttf
+curl https://raw.githubusercontent.com/google/fonts/main/ofl/sourcesanspro/SourceSansPro-BoldItalic.ttf -o SourceSansPro-BoldItalic.ttf
 
 python3 fontconvert.py regular_font 18 SourceSansPro-Regular.ttf --two-color --compress > ../lib/Fonts/regular_font.h
 python3 fontconvert.py bold_font 18 SourceSansPro-Bold.ttf --two-color --compress > ../lib/Fonts/bold_font.h
